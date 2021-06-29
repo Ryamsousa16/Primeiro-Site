77cbcd4111744a643ac1109210a052140965fcc9
Ryam Sousa <Ryam.sousa04@gmail.com>
2021-06-29 19:24:12 -0300

    Acrescentando o 'fale conosco' no site

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+window.alert("Olá professor, clicando em configurações vai estar a opção de 'fale conosco' e cairá na página," +
+ "já peço desculpas por não ter feito tão bem quanto o senhor...")
+
+function clicar(){
+	alert('Em breve!');
+}
+
